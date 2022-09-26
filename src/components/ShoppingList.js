a55ad4ec8277f2fb55cbf06ca5e04edb9f2afc4a
kapadokia <ktnyandoch@gmail.com>
2022-09-27 02:37:57 +0300

    completed useState to filter and change modes

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+   const [category, selectedCategory] = useState("All")
+
+   const filteredCategory = items.filter(cat =>{
+    if(category === "All") return true;
+    return cat.category.includes(category)
+   })
+
+   
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" value={category} onChange={e=> selectedCategory(e.target.value)}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,8 +23,8 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+        {filteredCategory.map((item) => (
+          <Item key={item.id} name={item.name} category={filteredCategory.category} />
         ))}
       </ul>
     </div>
